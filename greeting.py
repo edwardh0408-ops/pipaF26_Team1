6ec08124a66ec107e749976c88a3c1b2f4d48cbd
Arjun <As19112@nyu.edu>
2026-09-23 12:52:58 -0400

    add Arjun

--- greeting.py
+++ greeting.py
@@ -1,1 +1,1 @@
-print("Hello, World! Our group members are: [name1, name2, Kashish Satija, name4, name5]")
+print("Hello, World! Our group members are: [name1, name2, Kashish Satija, name4, Arjun]")
